e11e7979832694fabe9884667f0b545cfcc0fb19
paijipao123 <paijipao123@users.noreply.github.com>
2026-08-14 16:33:00 +0000

    修复 MainActivity 缺少 View import

--- app/src/main/java/com/wechathook/ui/MainActivity.kt
+++ app/src/main/java/com/wechathook/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.LinearLayout
